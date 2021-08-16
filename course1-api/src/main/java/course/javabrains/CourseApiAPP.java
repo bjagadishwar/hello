e4afb398f3e0f34fa6f68a351171a607ce5a0b0e
jagadishwar burra <85215548+bjagadishwar@users.noreply.github.com>
2021-08-16 20:34:41 +0530

    Add files via upload

---
+++ course1-api/src/main/java/course/javabrains/CourseApiAPP.java
@@ -0,0 +1,15 @@
+package course.javabrains;
+
+ import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiAPP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiAPP.class, args);
+		// TODO Auto-generated method stub
+
+	}
+
+}
